4353ef990eb4b78021887646c199cbbf1c35e755
VijKrRana <vij.kr.rana@outlook.com>
2020-05-14 22:26:34 +0530

    eureka codes.

---
+++ src/main/java/com/cis/eureka/CisEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.cis.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CisEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CisEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cis/eureka/CisEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cis.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CisEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
